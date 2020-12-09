58746cb53c044dad8d82c778a34739a71f7ba859
Dibyajyoti Panda <dibyajyoti.panda231@gmail.com>
2020-12-09 12:22:16 +0530

    Added environment variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/user-policy', {
+mongoose.connect(process.env.mongo_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
